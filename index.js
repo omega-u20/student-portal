6e3d8d2920fa8838483f8062c9f73caafa26de33
omega-u20 <udulaforactiveworlds@gmail.com>
2025-01-16 18:34:49 +0530

    udula-public-1_16

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.get('/',(req,res)=>{
     res.status(200)
 })
 
+app.post('/loging',(req,res)=>{
+    console.log(JSON.stringify(req.body));
+    
+})
+
 app.listen(PORT,()=>{
     console.log(`app alive on port:${PORT}`);
-    
 })
