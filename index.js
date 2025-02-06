3c02f2f08b21cc5817c52ca5bb5e0156ebaab6df
omega-u20 <udulaforactiveworlds@gmail.com>
2025-02-07 03:57:33 +0530

    pretty final4

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT||3000
 
+const fs = require('fs');
+fs.writeFile('built-time.js', `module.exports = '${new Date()}'`, (err) => {
+  if (err) throw err;
+  console.log('Build time file created successfully!');
+});
+
 app.use(express.static('public/'))
 
 app.get('/',(req,res)=>{
